e2329716839b8bd0206c73fd673d8f39ff3ad693
Matias Flores <arnod7991@gmail.com>
2020-07-01 16:17:07 -0400

    agregando docs

---
+++ docs/precache-manifest.6a5c133315035b23bdf22a61854335f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bdcd3051f3e65b8b348e347197922a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "139811e91a7b5ba46eeb",
+    "url": "/static/css/main.30704e85.chunk.css"
+  },
+  {
+    "revision": "cd451c5b8f399e525d02",
+    "url": "/static/js/2.5c9b66c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c9b66c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139811e91a7b5ba46eeb",
+    "url": "/static/js/main.4c5e534a.chunk.js"
+  },
+  {
+    "revision": "545a3457e694fbebd836",
+    "url": "/static/js/runtime-main.a10c141a.js"
+  }
+]);
